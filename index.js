8dd843f17bd8eed58a6f438798ae0d3b7625f008
T-nguyen-source <thomasnguyenmsw@GMAIL.COM>
2023-07-15 02:17:57 -0500

    added 1 round of rps

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ function getComputerChoice() {
 };
 
 console.log(getComputerChoice());
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+      return "tie!";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
+      return "you lose!";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
+      return "you lose!";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
+      return "you lose!";
+    } else {
+      return "you win!";
+    }
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  
